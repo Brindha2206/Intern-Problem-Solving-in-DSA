2fcbbe8eefc7a77821824f23e9b01382db06baa4
Brindha2206 <brindha220605@gmail.com>
2025-06-18 16:25:17 +0530

    Initial commit

---
+++ day1_deposits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class day1_deposits {
+   public static void main(String[] args) {
+      int data=0, deposits =0;
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("");
+
+      scanner.close();
+ while(data=20);
+ {
+    System.out.println("Enter the deposits of today:");
+    
+ }
+   }
+ 
+}
---
+++ tax.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class tax {
+    public static void main(String[] args) {
+        int income = 0, taxes= Integer.MIN_VALUE;
+        Scanner scanner = new Scanner(System.in);
+        while (income>=10) {
+            System.out.println("Enter your income:");
+            // if (income) {
+                
+            // }
+        }
+        System.out.println("The tax payers are");
+        scanner.close();
+    }
+    
+}
